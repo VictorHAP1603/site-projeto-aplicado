4298b703d78a30db3a4be3a639ceecd180ff27dc
VictorHugo1603 <vitor.hugo@newpost.com>
2021-10-07 22:31:09 -0300

    validando form

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,16 @@
 import PreencheComodidades from "./modules/cardsComodidades.js";
 import MenuMobile from "./modules/menuMobile.js";
 import Galeria from "./modules/slideGaleria.js";
+import ValidaForm from "./modules/validaForm.js"
 
-const preencheComodidades = new PreencheComodidades(
-    ".container-comodidades",
-    "#comodidades .comodidade__item"
-)
+const preencheComodidades = new PreencheComodidades(".container-comodidades", "#comodidades .comodidade__item")
 preencheComodidades.init()
 
-const galeria = new Galeria(
-    ".slide-imgs",
-    ".img-principal"
-)
+const galeria = new Galeria( ".slide-imgs",".img-principal")
 galeria.init();
-
 
 const menuMobile = new MenuMobile(".hamburguer", ".header-mb nav") 
 menuMobile.init()
+
+const validaForm = new ValidaForm("#contato form", '.form__input')
+validaForm.init();
---
+++ src/js/modules/validaForm.js
@@ -0,0 +1,110 @@
+const regexs = {
+    email: {
+        regex: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi,
+        message: 'Digite um e-mail válido'
+    }
+}
+
+export default class ValidaForm {
+    constructor(form, inputs) {
+        this.form = document.querySelector(form)
+        this.inputs = document.querySelectorAll(inputs)
+
+        this.buttonEnvia = document.querySelector(`${form} #submit`)
+    }
+
+    $$(els) { return document.querySelectorAll(els) }
+
+    submitForm(e) {
+        e.preventDefault();
+        
+        const isPreenchido = this.validaSeEstaoPreenchidos();
+        if (!isPreenchido) return;
+
+        const isValidos = this.validaSeSaoValidos();
+        if (!isValidos) return;
+
+        this.buttonEnvia.innerHTML = 'Enviando...';
+
+        setTimeout(() => {
+            const aviso = this.aviso('E-mail enviado com sucesso', 'message__send')
+            this.form.append(aviso);
+            this.buttonEnvia.innerHTML = 'Enviar'
+
+            this.inputs.forEach(i => i.value = '')
+
+            setTimeout(() => {
+                this.form.querySelector('.message__send').remove()
+            }, 3000)
+
+        }, 3000)
+
+
+    }
+
+    validaSeEstaoPreenchidos() {
+        let isValid = true;
+
+        this.inputs.forEach(i => {
+            if(i.value === '') {
+                i.classList.add('warning');
+                setTimeout(() => i.classList.remove('warning'), 2000)
+                
+                isValid = false;
+            } else {
+                i.classList.remove('warning');
+            }
+        })
+
+        return isValid
+    }
+
+    validaSeSaoValidos() {
+        let isValidos = true;
+
+        this.inputs.forEach(i => {
+            if (i.hasAttribute('data-regex')) {
+                const parentElement = i.parentNode;
+                const tipoRegex = i.getAttribute('data-regex');
+
+                if (!regexs[tipoRegex].regex.test(i.value)) {
+                    isValidos = false;
+                    i.classList.add('warning');
+
+                    const aviso = this.aviso(regexs[tipoRegex].message, 'message__invalid');
+
+                    if (!parentElement.querySelector('.message__invalid')) {
+                        parentElement.append(aviso);
+                    }
+                    
+                } else {
+                    if (parentElement.querySelector('.message__invalid')) {
+                        parentElement.querySelector('.message__invalid').remove();
+                        i.classList.remove('warning');
+                    }
+                }
+
+            }
+        })
+
+        return isValidos
+    }
+
+    aviso(text, classe) {
+        const span = document.createElement('span')
+        span.classList.add(classe)
+        span.innerHTML = text
+
+        return span;
+
+    }
+
+    bindEvents() {
+        this.submitForm = this.submitForm.bind(this)
+    }
+
+    init() {
+        this.bindEvents();
+        this.form.addEventListener('submit', this.submitForm)
+    }
+}
